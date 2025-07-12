812b0c95ed611f4cc7fae9f37c460c437a874484
yang <knisfp97@gmail.com>
2025-07-12 17:30:52 +0900

    CSS修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -7,59 +7,59 @@
 @section('content')
 
 <div class="product-list-page">
-<div class="product-page__header">
-    <h2 class="product-page__header-title">商品一覧</h2>
-    <div class="product-page__header-button">
-        <a href="/products/register">+ 商品を追加</a>
-    </div>
-</div>
-
-<div class="product-page">
-    <div class="product-page__search-form">
-        <form class="search-form" action="{{ route('products_search') }}" method="get">
-            <input type="text" name="text" placeholder="商品名で検索" value="{{ request('text') }}" />
-            <button>検索</button>
-
-            <div class="sort-form">
-                <h3 class="sort-form__header">価格順で表示</h3>
-                <select class="sort-form__select" name="sort" onchange="this.form.submit()">
-                    <option value="default">価格で並べ替え</option>
-                    <option value="hight" {{ request('sort') === 'hight' ? 'selected' : '' }}>高い順に表示</option>
-                    <option value="low" {{ request('sort') === 'low' ? 'selected' : '' }}>低い順に表示</option>
-                </select>
-
-                @if (request('sort') === 'hight' || request('sort') === 'low')
-                    <div class="sort-tags">
-                        @if (request('sort') === 'hight')
-                            <div class="sort-tags__hight">高い順に表示</div>
-                        @elseif (request('sort') === 'low')
-                            <div class="sort-tags__low">低い順に表示</div>
-                        @endif
-                        <button type="button" class="close" onclick="resetSort()">×</button>
-                    </div>
-                @endif
-            </div>
-        </form>
+    <div class="product-page__header">
+        <h2 class="product-page__header-title">商品一覧</h2>
+        <div class="product-page__header-button">
+            <a href="/products/register">+ 商品を追加</a>
+        </div>
     </div>
 
-    <div class="product-page__list">
-        @forelse ($products as $product)
-            <ul class="product-card">
-                <li>
-                    <a href="{{ route('product', ['productId' => $product->id]) }}" class="product-link">
-                        <img class="product-img" src="{{ asset($product->image) }}" alt="{{ $product->name }}" />
-                        <div class="product-tag">
-                            <span class="product-tag__name">{{ $product->name }}</span>
-                            <span class="product-tag__price">¥{{ number_format($product->price) }}</span>
+    <div class="product-page">
+        <div class="product-page__search-form">
+            <form class="search-form" action="{{ route('products_search') }}" method="get">
+                <input type="text" name="text" placeholder="商品名で検索" value="{{ request('text') }}" />
+                <button>検索</button>
+
+                <div class="sort-form">
+                    <h3 class="sort-form__header">価格順で表示</h3>
+                    <select class="sort-form__select" name="sort" onchange="this.form.submit()">
+                        <option value="default">価格で並べ替え</option>
+                        <option value="hight" {{ request('sort') === 'hight' ? 'selected' : '' }}>高い順に表示</option>
+                        <option value="low" {{ request('sort') === 'low' ? 'selected' : '' }}>低い順に表示</option>
+                    </select>
+
+                    @if (request('sort') === 'hight' || request('sort') === 'low')
+                        <div class="sort-tags">
+                            @if (request('sort') === 'hight')
+                                <div class="sort-tags__hight">高い順に表示</div>
+                            @elseif (request('sort') === 'low')
+                                <div class="sort-tags__low">低い順に表示</div>
+                            @endif
+                            <button type="button" class="close" onclick="resetSort()">×</button>
                         </div>
-                    </a>
-                </li>
-            </ul>
-        @empty
-            <p>該当する商品が見つかりませんでした。</p>
-        @endforelse
-    </div>
+                    @endif
+                </div>
+            </form>
+        </div>
 
+        <div class="product-page__list">
+            @forelse ($products as $product)
+                <ul class="product-card">
+                    <li>
+                        <a href="{{ route('product', ['productId' => $product->id]) }}" class="product-link">
+                            <img class="product-img" src="{{ asset($product->image) }}" alt="{{ $product->name }}" />
+                            <div class="product-tag">
+                                <span class="product-tag__name">{{ $product->name }}</span>
+                                <span class="product-tag__price">¥{{ number_format($product->price) }}</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            @empty
+                <p>該当する商品が見つかりませんでした。</p>
+            @endforelse
+        </div>
+    </div>
     {{-- ページネーション --}}
     <div class="pagination">
     @if ($products->lastPage() > 1)
@@ -86,7 +86,7 @@
         </div>
     @endif
     </div>
-</div>
+
 </div>
 {{-- 並び替えリセットボタン用スクリプト --}}
 <script>
